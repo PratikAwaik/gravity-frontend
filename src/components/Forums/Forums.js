957a79133d81cdd44e65895aa383f728e384626b
PratikAwaik <pratikawaik125@gmail.com>
2021-09-19 01:22:34 +0530

    user cannot create post without logging in

--- src/components/Forums/Forums.js
+++ src/components/Forums/Forums.js
@@ -6,17 +6,19 @@ import { Link } from "react-router-dom";
 
 const Forums = () => {
   const dispatch = useDispatch();
-  const forums = useSelector(state => state.forums);
+  const { forums, currentUser } = useSelector(state => state);
 
   useEffect(() => {
     dispatch(getAllPostsAction());
   }, [dispatch]);
 
+  const createPostLink = currentUser.username ? '/forums/create' : '/login';
+
   return (
     <div className="mt-16 forums-post-container pt-9 max-w-3xl mx-auto">
       <div className="flex items-center justify-between">
         <h2 className="text-2xl mb-2 px-2 py-2 rounded-md font-bold">Forums</h2>  
-        <Link to="/forums/create" className="border-2 border-theme-orange py-1 px-2 rounded-md hover:bg-theme-orange hover:text-theme-black">Create Post</Link>
+        <Link to={createPostLink} className="border-2 border-theme-orange py-1 px-2 rounded-md hover:bg-theme-orange hover:text-theme-black">Create Post</Link>
       </div>
       
       <hr className="mb-5" />
