a815d2d164ee4ace9b0bedf8b80d788818a932bd
PratikAwaik <pratikawaik125@gmail.com>
2021-09-19 23:51:28 +0530

    renamed cases for upvote and downvote

--- src/reducers/forums.js
+++ src/reducers/forums.js
@@ -2,9 +2,9 @@ const forumsReducer = (state = [], action) => {
   switch(action.type) {
     case 'GET_ALL_POSTS':
       return action.payload;
-    case 'UPVOTE_POST':
+    case 'UPVOTE_POST_FORUMS':
       return state.map(post => post.id.toString() === action.payload.id.toString() ? action.payload : post);
-    case 'DOWNVOTE_POST':
+    case 'DOWNVOTE_POST_FORUMS':
       return state.map(post => post.id.toString() === action.payload.id.toString() ? action.payload : post);
     case 'NEW_POST':
       return [...state, action.payload];
