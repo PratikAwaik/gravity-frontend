be2c0e07824391e11573be3f28b25c133cc3e2c7
PratikAwaik <pratikawaik125@gmail.com>
2021-09-19 23:51:52 +0530

    add post reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import currentUserReducer from "./currentUser";
 import forumsReducer from "./forums";
+import postReducer from "./post";
 
 const reducers = combineReducers({
   currentUser: currentUserReducer,
-  forums: forumsReducer
+  forums: forumsReducer,
+  post: postReducer
 });
 
 export default reducers;
