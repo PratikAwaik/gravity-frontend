d31430280fcadf46c18c8367ee31311ff478946a
PratikAwaik <pratikawaik125@gmail.com>
2021-09-19 23:55:58 +0530

    fetch user in forums component itself

--- src/components/Forums/Forums.js
+++ src/components/Forums/Forums.js
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ForumPost from './ForumPost';
 import { getAllPostsAction } from "../../actions/forums";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { getCurrentUserDetailsAction } from "../../actions/currentUser";
 
 const Forums = () => {
   const dispatch = useDispatch();
@@ -12,10 +13,14 @@ const Forums = () => {
     dispatch(getAllPostsAction());
   }, [dispatch]);
 
+  useEffect(() => {
+    if (currentUser.id) dispatch(getCurrentUserDetailsAction());
+  }, [dispatch, currentUser.id]);
+
   const createPostLink = currentUser.username ? '/forums/create' : '/login';
 
   return (
-    <div className="mt-16 forums-post-container pt-9 max-w-3xl mx-auto">
+    <div className="mt-16 forums-post-container pt-9 max-w-3xl mx-auto mb-16">
       <div className="flex items-center justify-between">
         <h2 className="text-2xl mb-2 px-2 py-2 rounded-md font-bold">Forums</h2>  
         <Link to={createPostLink} className="border-2 border-theme-orange py-1 px-2 rounded-md hover:bg-theme-orange hover:text-theme-black">Create Post</Link>
@@ -24,7 +29,7 @@ const Forums = () => {
       <hr className="mb-5" />
       
       <div className="forums-post-wrapper">
-        {forums.map(post => <ForumPost key={post.id} post={post} />)}
+        {forums.map(post => <ForumPost key={post.id} forums={forums} currentUser={currentUser} post={post} />)}
       </div>
     </div>
   );
