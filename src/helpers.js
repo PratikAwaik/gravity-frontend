963e82ca55fda81dcacf8e7026bcd6f85c2cd2c7
PratikAwaik <pratikawaik125@gmail.com>
2021-09-21 00:45:13 +0530

    removed editor config

--- src/helpers.js
+++ src/helpers.js
@@ -34,26 +34,3 @@ export const handleDownvoteHelper = async (dispatch, currentUser, post, hasUpvot
   await dispatch(downvoteAction(post.id, downvotesData));
   dispatch(getCurrentUserDetailsAction());
 }
-
-export const fancyToolbarConfig = {
-  options: [
-    'inline', 
-    'blockType', 
-    'list', 
-    'textAlign', 
-    'link', 
-    'embedded', 
-    'emoji', 
-    'image', 
-    'remove', 
-    'history'
-  ],
-  link: {
-    link: {
-      className: 'text-theme-orange underline'
-    }
-  },
-  image: {
-    previewImage: true
-  }
-}
