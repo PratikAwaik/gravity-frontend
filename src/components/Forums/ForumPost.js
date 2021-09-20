a5ad78ac8829709bb98d025cb1af526927967a1e
PratikAwaik <pratikawaik125@gmail.com>
2021-09-21 00:47:12 +0530

    refactoed code and added proptypes

--- src/components/Forums/ForumPost.js
+++ src/components/Forums/ForumPost.js
@@ -1,102 +1,29 @@
 import React from "react";
-import moment from "moment";
-import { Link } from "react-router-dom";
 import { handleDownvotesAction, handleUpvotesAction } from "../../actions/forums";
-import { useDispatch } from "react-redux";
-import draftToHtml from "draftjs-to-html";
-import DOMPurify from "dompurify";
-import MDEditor from "@uiw/react-md-editor";
-import { 
-  hasDownvotedAlreadyHelper, 
-  hasUpvotedAlreadyHelper,
-  handleUpvoteHelper,
-  handleDownvoteHelper
-} from "../../helpers";
+import PropTypes from 'prop-types';
+import PostHeader from "./PostHeader";
+import PostBody from "./PostBody";
+import PostFooter from "./PostFooter";
 
 const ForumPost = ({ post, currentUser }) => {
-  const dispatch = useDispatch();
-
-  const hasUpvotedAlready = hasUpvotedAlreadyHelper(currentUser, post.id);
-  const hasDownvotedAlready = hasDownvotedAlreadyHelper(currentUser, post.id);
-
-  const handleUpvoteClick = () => {
-    handleUpvoteHelper(dispatch, currentUser, post, hasUpvotedAlready, hasDownvotedAlready, handleUpvotesAction);
-  }
-
-  const handleDownvoteClick = () => {
-    handleDownvoteHelper(dispatch, currentUser, post, hasUpvotedAlready, hasDownvotedAlready, handleDownvotesAction);
-  }
-
   return (
     <div className="forum-post-container bg-theme-white border-4 border-theme-orange text-theme-black p-2 rounded-md mb-4 relative">
-      <div className="forum-post-header text-sm text-theme-gray mb-2 flex items-center">
-        Posted by 
-        {
-          post.user && post.user.username ? 
-          <Link to={`/user/${post.user.username}`} className="ml-1 underline z-10">{ post.user.username }</Link> :
-          <span className="ml-1 underline z-10">{'[deleted]'}</span>
-        }
-        <i className="ri-checkbox-blank-circle-fill mx-1 text-xs" style={{fontSize: '0.5rem'}}></i> 
-        { moment(post.createdAt).fromNow() }
-      </div>
-      <div className="forum-post-body max-h-60 overflow-hidden mb-2">
-        <Link to={`/forums/${post.id}`} className="forum-post-body-title">
-          <h3 className="text-xl font-bold mb-1">{post.title}</h3>
-        </Link>
-        
-        { post.type === 'editor' ?
-          <div 
-            className="text-base forum-post-body-content" 
-            dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(draftToHtml(JSON.parse(post.content))) }} 
-          /> :
-          <div className="text-base forum-post-content">
-            <MDEditor.Markdown source={post.content} />
-          </div>
-        }
-        
-      </div>
-      <div className="forum-post-footer flex items-center">
-        { currentUser.username ?
-          <div className="mr-2 flex items-center">
-            <i 
-              className={`ri-rocket-2-line cursor-pointer text-xl z-10 ${hasUpvotedAlready ? 'text-theme-purple' : ''}`}
-              onClick={handleUpvoteClick}
-            ></i>
-            {post.upvotes}
-          </div> :
-          <div>
-            <Link to="/login" className="mr-2 flex items-center">
-              <i 
-                className="ri-rocket-2-line cursor-pointer text-xl z-10"
-              ></i>
-              {post.upvotes}
-            </Link>
-          </div>
-        }
-
-        { currentUser.username ?
-          <div className="mr-4 flex items-center">
-            <i 
-              className={`ri-rocket-2-line transform rotate-180 cursor-pointer text-xl z-10 ${hasDownvotedAlready ? 'text-theme-red' : ''}`}
-              onClick={handleDownvoteClick}
-            ></i>
-            {post.downvotes}
-          </div> :
-          <Link to="/login" className="mr-4 flex items-center">
-            <i 
-              className="ri-rocket-2-line transform rotate-180 cursor-pointer text-xl z-10"
-            ></i> 
-            {post.downvotes}
-          </Link>
-        }
-        
-        <div className="flex items-center cursor-pointer z-10">
-          <i className="ri-chat-1-line mr-1 text-xl"></i>
-          comments
-        </div>
-      </div>
+      <PostHeader  post={post} />
+      <PostBody post={post} isPostDetail={false} />
+      <PostFooter
+        currentUser={currentUser}
+        post={post}
+        upvoteAction={handleUpvotesAction}
+        downvoteAction={handleDownvotesAction}
+        isPostDetail={false}
+      />
     </div>
   );
+}
+
+ForumPost.propTypes = {
+  post: PropTypes.object.isRequired,
+  currentUser: PropTypes.object.isRequired
 }
 
 export default ForumPost;
--- src/components/Forums/PostDetail.js
+++ src/components/Forums/PostDetail.js
@@ -1,23 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
-import moment from 'moment';
 import { Editor } from "react-draft-wysiwyg";
-import MDEditor from '@uiw/react-md-editor';
-import DOMPurify from 'dompurify';
-import draftToHtml from 'draftjs-to-html';
 import { handleDownvotesAction, handleUpvotesAction } from '../../actions/post';
 import { getSinglePostAction } from '../../actions/post';
 import { getCurrentUserDetailsAction } from '../../actions/currentUser';
-import { 
-  hasUpvotedAlreadyHelper, 
-  hasDownvotedAlreadyHelper,
-  handleUpvoteHelper, 
-  handleDownvoteHelper,
-  fancyToolbarConfig
-} from '../../helpers';
-
-// TODO: Separate post header, body, footer, comments in separate components 
+import PostHeader from './PostHeader';
+import PostBody from './PostBody';
+import PostFooter from './PostFooter';
+import fancyToolbarConfig from '../../editor.config';
 
 const PostDetail = () => {
   const [editorContent, setEditorContent] = useState('');
@@ -27,112 +18,44 @@ const PostDetail = () => {
 
   useEffect(() => {
     dispatch(getSinglePostAction(id));
-  }, [id, dispatch]);
+  }, [dispatch, id]);
 
   useEffect(() => {
     if (currentUser.id) dispatch(getCurrentUserDetailsAction());
   }, [dispatch, currentUser.id]);
 
-  const hasUpvotedAlready = hasUpvotedAlreadyHelper(currentUser, id);
-  const hasDownvotedAlready = hasDownvotedAlreadyHelper(currentUser, id);
-
-  const handleUpvoteClick = () => {
-    handleUpvoteHelper(dispatch, currentUser, post, hasUpvotedAlready, hasDownvotedAlready, handleUpvotesAction);
-  }
-
-  const handleDownvoteClick = () => {
-    handleDownvoteHelper(dispatch, currentUser, post, hasUpvotedAlready, hasDownvotedAlready, handleDownvotesAction);
-  }
-
   return (
     <div className="mt-24 mb-16 mx-auto max-w-3xl bg-theme-white  text-theme-black border-4 border-theme-orange rounded-md post-detail-container">
       <div className="p-2 post-detail-wrapper">
-        <div className="post-detail-header text-sm text-theme-gray mb-3 flex items-center">
-          Posted by 
-          {
-            post.user && post.user.username ? 
-            <Link to={`/user/${post.user.username}`} className="ml-1 underline z-10">{ post.user.username }</Link> :
-            <span className="ml-1 underline z-10">{'[deleted]'}</span>
-          }
-          <i className="ri-checkbox-blank-circle-fill mx-1 text-xs" style={{fontSize: '0.5rem'}}></i> 
-          { moment(post.createdAt).fromNow() }
-        </div>
+        <PostHeader post={post} />
+        <PostBody post={post} isPostDetail={true} />
+        <PostFooter
+          currentUser={currentUser}
+          post={post}
+          upvoteAction={handleUpvotesAction}
+          downvoteAction={handleDownvotesAction}
+          isPostDetail={true}
+        />
 
-        <div className="post-detail-body mb-3">
-          <div className="post-detail-body-title mb-2">
-            <h3 className="text-xl font-bold">{post.title}</h3>
+        {
+          currentUser.username &&
+          <div className="post-detail-add-comment mb-5">
+            <span className="text-sm mb-3">Comment as 
+              <Link className="ml-1 text-theme-orange underline" to={`/user/${currentUser.username}`}>{ currentUser.username }</Link>
+            </span>
+            <Editor
+              editorState={editorContent}
+              toolbarClassName="bg-theme-purple text-theme-black"
+              wrapperClassName="border-2 border-theme-purple rounded-md"
+              editorClassName="p-2"
+              toolbar={fancyToolbarConfig}
+              onEditorStateChange={(editorState) => setEditorContent(editorState)}
+              placeholder="What are your thoughts?"
+              handlePastedText={() => false} 
+            />
           </div>
-
-          <div className="post-detail-body-content">
-            { post.type === 'editor' ?
-              <div 
-                className="text-base forum-post-body-content" 
-                dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(draftToHtml(JSON.parse(post.content))) }} 
-              /> :
-              <div className="text-base forum-post-content">
-                <MDEditor.Markdown source={post.content} />
-              </div>
-            }
-          </div>
-        </div>
-
-        <div className="post-detail-footer mb-5 flex items-center">
-          { currentUser.username ?
-            <div className="mr-2 flex items-center">
-              <i 
-                className={`ri-rocket-2-line cursor-pointer text-xl z-10 ${hasUpvotedAlready ? 'text-theme-purple' : ''}`}
-                onClick={handleUpvoteClick}
-              ></i>
-              {post.upvotes}
-            </div> :
-            <div>
-              <Link to="/login" className="mr-2 flex items-center">
-                <i 
-                  className="ri-rocket-2-line cursor-pointer text-xl z-1}"
-                ></i>
-                {post.upvotes}
-              </Link>
-            </div>
-          }
-
-          { currentUser.username ?
-            <div className="mr-4 flex items-center">
-              <i 
-                className={`ri-rocket-2-line transform rotate-180 cursor-pointer text-xl z-10 ${hasDownvotedAlready ? 'text-theme-red' : ''}`}
-                onClick={handleDownvoteClick}
-              ></i>
-              {post.downvotes}
-            </div> :
-            <Link to="/login" className="mr-4 flex items-center">
-              <i 
-                className="ri-rocket-2-line transform rotate-180 cursor-pointer text-xl z-10"
-              ></i> 
-              {post.downvotes}
-            </Link>
-          }
-
-          <div className="flex items-center cursor-pointer z-10">
-            <i className="ri-chat-1-line mr-1 text-xl"></i>
-            comments
-          </div>
-        </div>
-
-        <div className="post-detail-add-comment mb-5">
-          <span className="text-sm mb-3">Comment as 
-            <Link className="ml-1 text-theme-orange underline" to={`/user/${currentUser.username}`}>{ currentUser.username }</Link>
-          </span>
-          <Editor
-            editorState={editorContent}
-            toolbarClassName="bg-theme-purple text-theme-black"
-            wrapperClassName="border-2 border-theme-purple rounded-md"
-            editorClassName="p-2"
-            toolbar={fancyToolbarConfig}
-            onEditorStateChange={(editorState) => setEditorContent(editorState)}
-            placeholder="What are your thoughts?"
-            handlePastedText={() => false} 
-          />
-        </div>
-
+        }
+        
         <div className="post-detail-comments"></div>
 
       </div>
