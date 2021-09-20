7bbe15c89d8f9f373ddceda56e53f862fbc29d7b
PratikAwaik <pratikawaik125@gmail.com>
2021-09-21 00:42:22 +0530

    Initial Commit

---
+++ src/editor.config.js
@@ -0,0 +1,24 @@
+const fancyToolbarConfig = {
+  options: [
+    'inline', 
+    'blockType', 
+    'list', 
+    'textAlign', 
+    'link', 
+    'embedded', 
+    'emoji', 
+    'image', 
+    'remove', 
+    'history'
+  ],
+  link: {
+    link: {
+      className: 'text-theme-orange underline'
+    }
+  },
+  image: {
+    previewImage: true
+  }
+}
+
+export default fancyToolbarConfig;
