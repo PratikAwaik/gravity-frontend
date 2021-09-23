cacc4e9ace6fa280187abe6090a9ba1f1c0f8a76
PratikAwaik <pratikawaik125@gmail.com>
2021-09-24 00:32:26 +0530

    removed errors case

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -4,33 +4,20 @@ const currentUserReducer = (state = {}, action) => {
       window.localStorage.setItem('loggedInGravityUser', JSON.stringify(action.payload));
       return action.payload;
     }
-    
-    case 'REGISTER_ERROR': {
-      return action.payload;
-    }
-      
     case 'LOGIN': {
       window.localStorage.setItem('loggedInGravityUser', JSON.stringify(action.payload));
       return action.payload;
     }
-      
-    case 'LOGIN_ERROR': {
-      return action.payload;
-    }
-      
     case 'LOGOUT': {
       window.localStorage.removeItem('loggedInGravityUser');
       return action.payload;
     }
-      
     case 'SET_FROM_LOCAL_STORAGE': {
       return action.payload;
     }
-
     case 'GET_USER_DETAILS': {
       return { ...state, ...action.payload };
     }
-      
     default:
       return state;
   }
