3a40f8fdd235787c985768580ec79224de3d07a9
PratikAwaik <pratikawaik125@gmail.com>
2021-09-24 00:28:06 +0530

    replaced services with dispatchers for better error handling

---
+++ src/dispatchers/forums.js
@@ -0,0 +1,96 @@
+import axios from "axios";
+import { setErrorAction } from "../actions/error";
+import { getSinglePostAction } from "../actions/post";
+import { 
+  createPostAction, 
+  deletePostAction, 
+  getAllPostsAction, 
+  handleDownvotesAction, 
+  handleUpvotesAction 
+} from "../actions/forums";
+
+const baseUrl = '/api/forums';
+
+export const getAllPostsDispatcher = async (dispatch) => {
+  try {
+    const response = await axios.get(baseUrl);
+    dispatch(getAllPostsAction(response.data));
+  } catch (err) {
+    console.log(err.response);
+    dispatch(setErrorAction(err.response.data));
+  }
+}
+
+export const getSinglePostDispatcher = async (dispatch, id) => {
+  try {
+    const response = await axios.get(`${baseUrl}/${id}`);
+    dispatch(getSinglePostAction(response.data));
+  } catch (err) {
+    console.log(err.response);
+    dispatch(setErrorAction(err.response.data));
+  }
+} 
+
+export const handleUpvoteDispatcher = async (dispatch, id, upvotesData, userToken) => {
+  const config = {
+    headers: {
+      Authorization: 'Bearer ' + userToken
+    }
+  }
+
+  try {
+    const response = await axios.patch(`${baseUrl}/${id}/upvote`, upvotesData, config);
+    dispatch(handleUpvotesAction(response.data));
+  } catch (err) {
+    console.log(err.response);
+    dispatch(setErrorAction(err.response.data));
+  }
+}
+
+export const handleDownvoteDispatcher = async (dispatch, id, downvotesData, userToken) => {
+  const config = {
+    headers: {
+      Authorization: 'Bearer ' + userToken
+    }
+  }
+
+  try {
+    const response = await axios.patch(`${baseUrl}/${id}/downvote`, downvotesData, config);
+    dispatch(handleDownvotesAction(response.data));
+  } catch (err) {
+    console.log(err.response);
+    dispatch(setErrorAction(err.response.data));
+  }
+}
+
+export const createPostDispatcher = async (dispatch, postData, userToken) => {
+  const config = {
+    headers: {
+      Authorization: 'Bearer ' + userToken
+    }
+  }
+
+  try {
+    const response = await axios.post(baseUrl, postData, config);
+    dispatch(createPostAction(response.data));
+  } catch (err) {
+    console.log(err.response);
+    dispatch(setErrorAction(err.response.data));
+  }
+}
+
+export const deletePostDispatcher = async (dispatch, id, userToken) => {
+  const config = {
+    headers: {
+      Authorization: 'Bearer ' + userToken
+    }
+  }
+
+  try {
+    await axios.delete(`${baseUrl}/${id}`, config); 
+    dispatch(deletePostAction(id));
+  } catch (err) {
+    console.log(err.response);
+    dispatch(setErrorAction(err.response.data));
+  }
+}
---
+++ src/dispatchers/user.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { getCurrentUserDetailsAction, loginUserAction, registerUserAction } from "../actions/currentUser";
+import { setErrorAction } from "../actions/error";
+
+const baseUrl = '/api/users';
+
+export const getSingleUserDispatcher = async (dispatch, id) => {
+  try {
+    const response = await axios.get(`${baseUrl}/${id}`);
+    dispatch(getCurrentUserDetailsAction(response.data));
+  } catch (err) {
+    dispatch(setErrorAction(err.response.data));
+  }
+}
+
+export const registerUserDispatcher = async (dispatch, userInfo) => {
+  try {
+    const response = await axios.post(`${baseUrl}/register`, userInfo);
+    dispatch(registerUserAction(response.data));
+  } catch (err) {
+    console.log(err.response);
+    dispatch(setErrorAction(err.response.data));
+  }
+}
+
+export const loginUserDispatcher = async (dispatch, userInfo) => {
+  try {
+    const response = await axios.post(`${baseUrl}/login`, userInfo);
+    console.log(response.data);
+    dispatch(loginUserAction(response.data));
+  } catch (err) {
+    console.log(err.response);
+    dispatch(setErrorAction(err.response.data));
+  }
+}
+
+export const currentUserDetailsDispatcher = async (dispatch) => {
+  const { id } = JSON.parse(window.localStorage.getItem('loggedInGravityUser'));
+  try {
+    const response = await axios.get(`${baseUrl}/${id}`);
+    dispatch(getCurrentUserDetailsAction(response.data));
+  } catch (err) {
+    console.log(err.response);
+    dispatch(setErrorAction(err.response.data));
+  }
+}
