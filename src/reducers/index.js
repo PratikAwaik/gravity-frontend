46a1537d18cc4a8f462bb8271285de446e4d3178
PratikAwaik <pratikawaik125@gmail.com>
2021-09-24 16:54:38 +0530

    removed postReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from "redux";
 import currentUserReducer from "./currentUser";
 import errorReducer from "./error";
 import forumsReducer from "./forums";
-import postReducer from "./post";
 
 const reducers = combineReducers({
   currentUser: currentUserReducer,
   forums: forumsReducer,
-  post: postReducer,
   error: errorReducer
 });
 
