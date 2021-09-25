bf652b23cf73c38d9654860dd11d5403e8669db4
PratikAwaik <pratikawaik125@gmail.com>
2021-09-26 00:57:02 +0530

    add config for CKEditor

--- src/configs/ckEditorConfig.js
+++ src/configs/ckEditorConfig.js
@@ -1,4 +1,44 @@
 const ckEditorConfig = {
+  toolbar: {
+    items: [
+      'heading',
+      '|',
+      'bold',
+      'italic',
+      'underline',
+      'link',
+      'strikethrough',
+      'code',
+      'codeBlock',
+      'bulletedList',
+      'numberedList',
+      '|',
+      'outdent',
+      'indent',
+      '|',
+      'blockQuote',
+      'insertTable',
+      'mediaEmbed',
+      'undo',
+      'redo'
+    ]
+  },
+  language: 'en',
+  image: {
+    toolbar: [
+      'imageTextAlternative',
+      'imageStyle:inline',
+      'imageStyle:block',
+      'imageStyle:side'
+    ]
+  },
+  table: {
+    contentToolbar: [
+      'tableColumn',
+      'tableRow',
+      'mergeTableCells'
+    ]
+  },
   mediaEmbed: {
     previewsInData: true
   },
