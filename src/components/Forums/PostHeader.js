c729ba84c43b8c81500cff36dc21660b491b520d
PratikAwaik <pratikawaik125@gmail.com>
2021-09-26 22:00:09 +0530

    UI changes

--- src/components/Forums/PostHeader.js
+++ src/components/Forums/PostHeader.js
@@ -5,23 +5,21 @@ import PropTypes from "prop-types";
 
 const PostHeader = ({ post }) => {
   return (
-    <div className="forum-post-header text-theme-gray text-sm mb-2 flex items-center">
+    <div className="forum-post-header text-theme-gray text-sm mb-2 flex items-center z-10">
       Posted by
       {post.user && post.user.username ? (
-        <Link
-          to={`/user/${post.user.username}`}
-          className="ml-1 underline z-10"
-        >
+        <Link to={`/user/${post.user.username}`} className="ml-1 underline">
           {post.user.username}
         </Link>
       ) : (
-        <span className="ml-1 underline z-10">{"[deleted]"}</span>
+        <span className="ml-1 underline">{"[deleted]"}</span>
       )}
       <i
         className="ri-checkbox-blank-circle-fill mx-1 text-xs"
         style={{ fontSize: "0.5rem" }}
       ></i>
       {moment(post.createdAt).fromNow()}
+      {post.editedAt && <span className="ml-1">(edited)</span>}
     </div>
   );
 };
