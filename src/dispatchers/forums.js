f9e4225dfa245ce40196c6c3a11c8d9bdb1af068
PratikAwaik <pratikawaik125@gmail.com>
2021-09-30 00:31:59 +0530

    add createComment and handleCommentUpvote dispatchers

--- src/dispatchers/forums.js
+++ src/dispatchers/forums.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { setErrorAction } from "../actions/error";
 import {
+  createCommentAction,
   createPostAction,
   deletePostAction,
   editPostAction,
   getAllCommentsAction,
   getAllPostsAction,
+  handleCommentUpvoteAction,
   // getSinglePostAction,
   handleDownvotesAction,
   handleUpvotesAction,
@@ -144,6 +146,57 @@ export const getAllCommentsDispatcher = async (dispatch, postId) => {
   try {
     const response = await axios.get(`${baseUrl}/${postId}/comments`);
     dispatch(getAllCommentsAction(response.data, postId));
+  } catch (err) {
+    console.log(err.response);
+    dispatch(setErrorAction(err.response.data));
+  }
+};
+
+export const createCommentDispatcher = async (
+  dispatch,
+  postId,
+  userToken,
+  commentData
+) => {
+  const config = {
+    headers: {
+      Authorization: "Bearer " + userToken,
+    },
+  };
+
+  try {
+    const response = await axios.post(
+      `${baseUrl}/${postId}/comments`,
+      commentData,
+      config
+    );
+    dispatch(createCommentAction(response.data, postId));
+  } catch (err) {
+    console.log(err.response);
+    dispatch(setErrorAction(err.response.data));
+  }
+};
+
+export const handleCommentUpvoteDispatcher = async (
+  dispatch,
+  postId,
+  commentId,
+  upvotesData,
+  userToken
+) => {
+  const config = {
+    headers: {
+      Authorization: "Bearer " + userToken,
+    },
+  };
+
+  try {
+    const response = await axios.patch(
+      `${baseUrl}/${postId}/comments/${commentId}/upvote`,
+      upvotesData,
+      config
+    );
+    dispatch(handleCommentUpvoteAction(response.data, postId));
   } catch (err) {
     console.log(err.response);
     dispatch(setErrorAction(err.response.data));
