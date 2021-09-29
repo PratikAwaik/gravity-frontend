c51f89557d5d5f52feb0e988924d4c8994a3bf31
PratikAwaik <pratikawaik125@gmail.com>
2021-09-30 00:34:46 +0530

    changed config for comments

--- src/components/Editors/FancyEditor.js
+++ src/components/Editors/FancyEditor.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "ckeditor5-build-classic-autoimage";
-import ckEditorConfig from "../../configs/ckEditorConfig";
+import PropTypes from "prop-types";
+import editorConfig from "../../configs/ckEditorConfig";
 
-const FancyEditor = ({ editorContent, setEditorContent }) => {
+const FancyEditor = ({ editorContent, setEditorContent, isPost }) => {
   const handleChange = (event, editor) => {
     const data = editor.getData();
     setEditorContent(data);
@@ -14,9 +15,19 @@ const FancyEditor = ({ editorContent, setEditorContent }) => {
       editor={ClassicEditor}
       data={editorContent}
       onChange={handleChange}
-      config={ckEditorConfig}
+      config={
+        isPost
+          ? editorConfig.ckEditorConfigPost
+          : editorConfig.ckEditorConfigComment
+      }
     />
   );
+};
+
+FancyEditor.propTypes = {
+  editorContent: PropTypes.string.isRequired,
+  setEditorContent: PropTypes.func.isRequired,
+  isPost: PropTypes.bool.isRequired,
 };
 
 export default FancyEditor;
