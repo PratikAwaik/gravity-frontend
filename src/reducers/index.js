f54d4edb68749be2446e6ede1c01ead2193146ca
PratikAwaik <pratikawaik125@gmail.com>
2021-09-30 23:51:30 +0530

    add global state [comments]

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import currentUserReducer from "./currentUser";
 import errorReducer from "./error";
 import forumsReducer from "./forums";
+import commentsReducer from "./comments";
 
 const reducers = combineReducers({
   currentUser: currentUserReducer,
   forums: forumsReducer,
+  comments: commentsReducer,
   error: errorReducer,
 });
 
