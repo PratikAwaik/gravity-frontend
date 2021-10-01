763a2d07281a1d011a60699e1c896f4c6a7658fe
PratikAwaik <pratikawaik125@gmail.com>
2021-10-02 01:03:48 +0530

    add FancyEditor for replies

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import CommentHeader from "./CommentHeader";
 import CommentBody from "./CommentBody";
 import CommentFooter from "./CommentFooter";
+import FancyEditor from "../Editors/FancyEditor";
+import { createCommentDispatcher } from "../../dispatchers/comments";
+import { useDispatch, useSelector } from "react-redux";
 
 const Comment = ({ comment }) => {
+  const [replyClicked, setReplyClicked] = useState(false);
+  const [editorContent, setEditorContent] = useState("");
+  const [showReplies, setShowReplies] = useState(false);
+  const currentUser = useSelector((state) => state.currentUser);
+  const comments = useSelector((state) => state.comments);
+  const dispatch = useDispatch();
+
+  const handleCreateComment = async (e) => {
+    e.preventDefault();
+    const commentData = {
+      content: editorContent,
+      repliedTo: comment.id,
+    };
+    await createCommentDispatcher(
+      dispatch,
+      comment.post,
+      currentUser.token,
+      commentData
+    );
+    setEditorContent("");
+  };
+
+  console.log(comment);
+
   return (
     <div className="flex flex-col mb-5 comment">
       <CommentHeader comment={comment} />
       <CommentBody comment={comment} />
-      <CommentFooter comment={comment} />
+      <CommentFooter
+        comment={comment}
+        setReplyClicked={setReplyClicked}
+        setShowReplies={setShowReplies}
+      />
+      {replyClicked && (
+        <div className="pl-6 my-4">
+          <FancyEditor
+            editorContent={editorContent}
+            setEditorContent={setEditorContent}
+            isPost={false}
+          />
+
+          <button type="button" onClick={handleCreateComment}>
+            Comment
+          </button>
+        </div>
+      )}
     </div>
   );
 };
