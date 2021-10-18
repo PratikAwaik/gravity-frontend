bae8c106fa27e80add562cbcf53abe7983f7dc75
PratikAwaik <pratikawaik125@gmail.com>
2021-10-18 18:41:37 +0530

    replaced heroicon components with svgs

--- src/components/Navbar/ProfileDropdown.js
+++ src/components/Navbar/ProfileDropdown.js
@@ -1,6 +1,5 @@
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
-import { ChevronDownIcon, UserCircleIcon } from "@heroicons/react/solid";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -13,12 +12,38 @@ const ProfileDropdown = ({ handleSignOut }) => {
   return (
     <Menu as="div" className="relative inline-block text-left mx-2">
       <div>
-        <Menu.Button className="inline-flex items-center justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-0.5 bg-white text-sm font-medium text-gray-700 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-theme-green">
-          <UserCircleIcon className="w-7 h-7" />
-          <ChevronDownIcon
-            className="w-5 h-5 ml-2 -mr-1 mt-1 text-violet-200 hover:text-violet-100"
-            aria-hidden="true"
-          />
+        <Menu.Button className="inline-flex items-center justify-center w-full rounded-md p-1 px-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-300">
+          {/* User Icon */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-7 w-7"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+
+          {/* Chevron Down Icon */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 ml-2 -mr-1 mt-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
         </Menu.Button>
       </div>
 
@@ -42,7 +67,20 @@ const ProfileDropdown = ({ handleSignOut }) => {
                     "px-4 py-2 text-sm flex items-center"
                   )}
                 >
-                  <i className="ri-profile-line text-lg mr-2"></i>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 mr-2"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"
+                    />
+                  </svg>
                   <span className="text-sm">Profile</span>
                 </Link>
               )}
@@ -56,7 +94,21 @@ const ProfileDropdown = ({ handleSignOut }) => {
                     "px-4 py-2 text-sm flex items-center"
                   )}
                 >
-                  <i className="ri-community-line text-lg mr-2"></i>
+                  {/* <i className="ri-community-line text-lg mr-2"></i> */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 mr-2"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
+                    />
+                  </svg>
                   <span className="text-sm">Create Community</span>
                 </Link>
               )}
@@ -71,7 +123,21 @@ const ProfileDropdown = ({ handleSignOut }) => {
                   )}
                   onClick={handleSignOut}
                 >
-                  <i className="ri-logout-box-r-line text-lg mr-2"></i>
+                  {/* <i className="ri-logout-box-r-line text-lg mr-2"></i> */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 mr-2"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                    />
+                  </svg>
                   <span className="text-sm">Sign Out</span>
                 </button>
               )}
