078bd437a130788f521c5dcc802855f948f56f8a
PratikAwaik <pratikawaik125@gmail.com>
2021-10-18 18:40:44 +0530

    add subreddit

--- src/components/Forums/PostHeader.js
+++ src/components/Forums/PostHeader.js
@@ -6,6 +6,13 @@ import PropTypes from "prop-types";
 const PostHeader = ({ post }) => {
   return (
     <div className="forum-post-header text-theme-gray text-sm mb-2 flex items-center z-10">
+      <Link
+        to={`/r/${post.subreddit.id}`}
+        className="flex items-center mr-2 hover:underline"
+      >
+        <div className="w-4 h-4 rounded-full bg-theme-black mr-1"></div>
+        <span className="font-bold">{post.subreddit.name}</span>
+      </Link>
       Posted by
       {post.user && post.user.username ? (
         <Link to={`/user/${post.user.username}`} className="ml-1 underline">
@@ -14,10 +21,7 @@ const PostHeader = ({ post }) => {
       ) : (
         <span className="ml-1 underline">{"[deleted]"}</span>
       )}
-      <i
-        className="ri-checkbox-blank-circle-fill mx-1 text-xs"
-        style={{ fontSize: "0.5rem" }}
-      ></i>
+      <div className="mx-2 inline-block w-1 h-1 bg-theme-black rounded-full"></div>
       {moment(post.createdAt).fromNow()}
       {post.editedAt && <span className="ml-1">(edited)</span>}
     </div>
