a1edb18b90cacf15bbdd93c68d3e2dfced27f78f
PratikAwaik <pratikawaik125@gmail.com>
2021-10-24 01:23:02 +0530

    removed comments

--- src/components/Forums/Forums.js
+++ src/components/Forums/Forums.js
@@ -13,8 +13,8 @@ const Forums = () => {
 
   useEffect(() => {
     (async function () {
-      await getAllPostsDispatcher(dispatch);
       scrollToPreviousPosition();
+      await getAllPostsDispatcher(dispatch);
     })();
   }, [dispatch]);
 
@@ -32,18 +32,6 @@ const Forums = () => {
 
   return (
     <div className="mt-16 forums-post-container pt-9 max-w-4xl mx-auto mb-16">
-      {/* <div className="flex items-center justify-between">
-        <h2 className="text-2xl mb-2 px-2 py-2 rounded-md font-bold">Forums</h2>
-        <Link
-          to="/forums/create"
-          className="border-2 border-theme-green py-1 px-2 rounded-md hover:bg-theme-green hover:text-theme-white"
-        >
-          Create Post
-        </Link>
-      </div>
-
-      <hr className="mb-5" /> */}
-
       <div className="forums-post-wrapper">
         {forums.map((post) => (
           <React.Suspense key={post.id}>
