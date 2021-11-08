0a91e495f6ae2439c386eaac470c71b612762818
PratikAwaik <pratikawaik125@gmail.com>
2021-11-08 16:52:21 +0530

    add error and extraProps

--- src/components/Auth/FormInput.js
+++ src/components/Auth/FormInput.js
@@ -1,21 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const FormInput = ({ type, name, label, value, handleChange }) => {
+const FormInput = ({
+  type,
+  name,
+  label,
+  value,
+  handleChange,
+  error,
+  extraProps,
+}) => {
   return (
     <div className="flex flex-col items-start w-full">
-      <label htmlFor={name} className="mb-1">
+      <label htmlFor={name} className="mb-1 font-bold">
         {label}
       </label>
       <input
         autoComplete="off"
-        className="w-full p-2 bg-transparent border-2 rounded-md border-theme-green outline-none focus-within::bg-transparent"
+        className={`w-full p-2 bg-transparent border-2 rounded-md ${
+          error ? "border-theme-red" : "border-theme-green"
+        } outline-none focus-within::bg-transparent`}
         type={type}
         name={name}
         id={name}
         value={value}
         onChange={handleChange}
         required
+        {...extraProps}
       />
     </div>
   );
@@ -27,6 +38,8 @@ FormInput.propTypes = {
   label: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired,
+  error: PropTypes.string,
+  extraProps: PropTypes.object,
 };
 
 export default FormInput;
