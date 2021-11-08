dbf2ebcb41b818e0047ab88aca5daa921463870e
PratikAwaik <pratikawaik125@gmail.com>
2021-11-08 16:38:50 +0530

    Initial Commit

---
+++ src/components/Post/EditPost.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import FancyEditor from "../Editors/FancyEditor";
+import { editPostDispatcher } from "../../dispatchers/forums";
+import { successPopup } from "../../helpers";
+
+const EditPost = ({ post, setPost, setToEdit }) => {
+  const [editorState, setEditorState] = useState(post.content);
+  const currentUser = useSelector((state) => state.currentUser);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await editPostDispatcher(
+      dispatch,
+      id,
+      { content: editorState },
+      currentUser.token
+    );
+    setPost({ ...post, content: editorState });
+    await successPopup("Post updated successfully!");
+    setToEdit(false);
+  };
+
+  return (
+    <div className="mt-5 create-post-container pt-9 max-w-3xl mx-auto mb-5">
+      <form onSubmit={handleSubmit}>
+        <FancyEditor
+          editorContent={editorState}
+          setEditorContent={setEditorState}
+          isPost={true}
+        />
+
+        <div className="flex items-center mt-5">
+          <button
+            type="submit"
+            className="px-5 py-2 border-2 border-theme-green rounded-md hover:bg-theme-green hover:text-theme-white"
+          >
+            Update
+          </button>
+          <button
+            type="button"
+            className="ml-4 px-5 py-2 border-2 border-theme-red rounded-md hover:bg-theme-red"
+            onClick={() => {
+              setToEdit(false);
+            }}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+EditPost.propTypes = {
+  post: PropTypes.object.isRequired,
+  setPost: PropTypes.func.isRequired,
+  setToEdit: PropTypes.func.isRequired,
+};
+
+export default EditPost;
---
+++ src/components/Post/PostDetail.js
@@ -0,0 +1,117 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams, useHistory } from "react-router-dom";
+import { createCommentDispatcher } from "../../dispatchers/comments";
+import PostHeader from "./PostHeader";
+import PostBody from "./PostBody";
+import PostFooter from "./PostFooter";
+import FancyEditor from "../Editors/FancyEditor";
+import { currentUserDetailsDispatcher } from "../../dispatchers/user";
+import { getAllPostsDispatcher } from "../../dispatchers/forums";
+
+const Comments = React.lazy(() => import("../Comments/Comments"));
+
+const PostDetail = () => {
+  const [editorContent, setEditorContent] = useState("");
+  const [post, setPost] = useState({});
+  const [toEdit, setToEdit] = useState(false);
+  const { id } = useParams();
+  const { currentUser, forums } = useSelector((state) => state);
+  const iter = useRef(0);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    (async function () {
+      if (
+        (forums.length === 0 || !forums.find((post) => post.id === id)) &&
+        iter < 1
+      ) {
+        await getAllPostsDispatcher(dispatch);
+        iter.current += 1;
+      } else {
+        const post = forums.find((post) => post.id === id);
+        if (post) {
+          setPost(post);
+        } else {
+          history.replace("/404");
+        }
+      }
+    })();
+
+    return () => setPost({});
+  }, [dispatch, forums, id, history]);
+
+  useEffect(() => {
+    if (currentUser.id) currentUserDetailsDispatcher(dispatch);
+  }, [dispatch, currentUser.id]);
+
+  const handleCreateComment = async () => {
+    const commentData = {
+      content: editorContent,
+      level: 0,
+    };
+    await createCommentDispatcher(
+      dispatch,
+      post.id,
+      currentUser.token,
+      commentData
+    );
+    setEditorContent("");
+  };
+
+  return post && post.id ? (
+    <div className="mt-24 mb-16 mx-auto max-w-4xl bg-transparent border-2 rounded-md shadow-md post-detail-container">
+      <div className="p-4 post-detail-wrapper">
+        <PostHeader post={post} />
+        <PostBody
+          post={post}
+          setPost={setPost}
+          isPostDetail={true}
+          toEdit={toEdit}
+          setToEdit={setToEdit}
+        />
+        <PostFooter
+          post={post}
+          isPostDetail={true}
+          toEdit={toEdit}
+          setToEdit={setToEdit}
+        />
+
+        {currentUser.username && (
+          <div className="post-detail-add-comment mb-5">
+            <span className="block text-sm mb-3 text-theme-gray">
+              Comment as
+              <Link className="ml-1 underline" to={`/user/${currentUser.id}`}>
+                {currentUser.username}
+              </Link>
+            </span>
+
+            <FancyEditor
+              editorContent={editorContent}
+              setEditorContent={setEditorContent}
+              isPost={false}
+            />
+
+            <button
+              type="button"
+              className="mt-2 px-4 py-1 border-2 border-theme-green rounded-md hover:bg-theme-green hover:text-theme-white"
+              onClick={handleCreateComment}
+            >
+              Comment
+            </button>
+          </div>
+        )}
+
+        <hr />
+        <div className="my-4 post-detail-comments">
+          <React.Suspense>
+            <Comments post={post} />
+          </React.Suspense>
+        </div>
+      </div>
+    </div>
+  ) : null;
+};
+
+export default PostDetail;
