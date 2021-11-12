987f62064d0ffca19d692135d9b71d9f1cc3a8fb
PratikAwaik <pratikawaik125@gmail.com>
2021-11-12 17:05:36 +0530

    changed width and height to 100%

--- src/components/Utils/LoadingWrapper.js
+++ src/components/Utils/LoadingWrapper.js
@@ -3,7 +3,7 @@ import loadingIcon from "../../images/loading-icon.gif";
 
 const LoadingWrapper = ({ loading, children }) => {
   return loading ? (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div className="w-full h-full flex items-center justify-center">
       <img className="w-10 h-10" src={loadingIcon} alt="Loading Icon" />
     </div>
   ) : (
