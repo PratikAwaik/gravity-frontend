965f2a63ed7ad0da3789a4c796aef6b9dda6b7b5
PratikAwaik <pratikawaik125@gmail.com>
2022-10-02 16:06:10 +0530

    remove app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl text-blue-700">Gravity</h1>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
