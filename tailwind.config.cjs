c240aa02f1424749a328a324b5b951f6d0ee6b13
PratikAwaik <pratikawaik125@gmail.com>
2022-10-02 16:03:16 +0530

    add css and tailwind initial styles and color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "theme-gray-400": "#242728",
+        "theme-gray-300": "#242424",
+        "theme-gray-200": "#5E6366", //
+        "theme-blue": "#367bf5", // icons when clicked or hovered or buttons
+        "theme-red": "#f26771",
+        "theme-green": "#B8EF81",
+        "theme-white-500": "#e1e1e1",
+        "theme-white-400": "#c6cacc", // icons
+        "theme-white-300": "#f3f3f4",
+      },
+      fontFamily: {
+        "theme-font-family": "'Roboto', sans-serif",
+      },
+    },
   },
   plugins: [],
 };
