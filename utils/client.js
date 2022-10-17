35ab4c9a0e0728a8ebf784d840b0ce75d986a650
PratikAwaik <pratikawaik125@gmail.com>
2022-10-17 23:58:10 +0530

    switch to yarn, fix backend local uri for apollo client

--- utils/client.js
+++ utils/client.js
@@ -11,7 +11,7 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-const httpLink = new HttpLink({ uri: "http://localhost:3001" });
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
